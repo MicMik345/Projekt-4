a1dc1273c7327742a9c8cc623af80fcc6b7b493f
MicMik345 <177254@stud.prz.edu.pl>
2025-04-23 12:36:46 +0000

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,14 @@ Kod bazowy programu Commit4_0:
 • Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
 • Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
 */
-
+import java.util.Scanner; 
 import java.io.IOException;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      
 
       var students = s.getStudents();
       for(Student current : students) {
